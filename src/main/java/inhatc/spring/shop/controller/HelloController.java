ddd5dac59a67f2216bc0435f9779fc9c56097011
Lee UngHoe <leewh0829@gmail.com>
2023-10-04 12:22:24 +0900

    2023 / 10 / 04 스프링프레임워크 수업 타임리프로 레이아웃 사용, 경로 지정

--- src/main/java/inhatc/spring/shop/controller/HelloController.java
+++ src/main/java/inhatc/spring/shop/controller/HelloController.java
@@ -1,18 +1,16 @@
 package inhatc.spring.shop.controller;
 
 import inhatc.spring.shop.dto.UserDto;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public UserDto hello(){
-        UserDto userDto = new UserDto();
-        userDto.setAge(20);
-        userDto.setName("홍길동");
+    public String index(){
 
-        return userDto;
+        return "index";
     }
 }
--- src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
+++ src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
@@ -1,12 +1,36 @@
 package inhatc.spring.shop.controller;
 
 import inhatc.spring.shop.dto.ItemDto;
+import inhatc.spring.shop.dto.ParamDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class ThymeleafController {
+
+    @GetMapping("/thymeleaf/temp")
+    public String temp() {
+
+        return "thymeleaf/temp";
+    }
+
+    @GetMapping("/thymeleaf/ex3")
+    public String ex3(ParamDto dto, Model model) {
+
+        log.info("==========================> ParamDto : " + dto);
+        model.addAttribute("param1", dto.getParam1());
+        model.addAttribute("param2", dto.getParam2());
+        return "thymeleaf/ex3";
+    }
+
+    @GetMapping("/thymeleaf/ex2")
+    public String ex2() {
+
+        return "thymeleaf/ex2";
+    }
 
     @GetMapping("/thymeleaf/ex1")
     public String ex1(Model model) {
